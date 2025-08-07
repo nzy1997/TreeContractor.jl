bb1cc4547f004af413d238dcb9e68decb569197a
Documenter.jl <documenter@juliadocs.github.io>
2025-08-07 04:31:59 +0000

    build based on 5c903c6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#TreeContractor","page":"Home","title":"TreeContractor","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TreeContractor.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
